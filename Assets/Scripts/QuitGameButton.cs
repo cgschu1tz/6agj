cb7f81b474e0376e12617e031dbdbc4a48a331ed
LukeFalk98 <lukie.babookie@gmail.com>
2020-02-15 13:29:38 -0600

    Added Title Screen and Buttons

---
+++ Assets/Scripts/QuitGameButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitGameButton : MonoBehaviour
+{
+    private void OnMouseUp()
+    {
+        Application.Quit(0);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/StartGameButton.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGameButton : MonoBehaviour
+{
+    private void OnMouseUp()
+    {
+        Application.LoadLevel(1);
+    }
+}
