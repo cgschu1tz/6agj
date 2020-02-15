a1fd1067766ec378c65f5c185a6b98f76957f40a
cgschu1tz <cgschultz@ksu.edu>
2020-02-15 00:04:45 -0600

    Before blowing it all up.

---
+++ Assets/Source/Universe.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Universe : MonoBehaviour
+{
+    public Tile[,] BelowGround { get; private set; }
+
+    void Awake()
+    {
+        Debug.Log("Hello, universe.");
+    }
+}
