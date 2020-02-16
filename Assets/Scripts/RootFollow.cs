bda7ed5e82a9ea99e99c943a035e0916dabb594e
LukeFalk98 <lukie.babookie@gmail.com>
2020-02-15 23:27:12 -0600

    Post hoc ergo propter hoc.

--- Assets/Scripts/RootFollow.cs
+++ Assets/Scripts/RootFollow.cs
@@ -2,43 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RootFollow : MonoBehaviour
+namespace Assets.Scripts
 {
-    Vector3 rootPosition;
-    public float moveSpeed = 0.1f;
-    Vector2 position = new Vector2(0f, 0f);
-    Rigidbody2D rb;
-    Transform Hub;
-    GameObject root;
-    public float heightLimit;
+    public class RootFollow : MonoBehaviour
+    {
+        Vector3 rootPosition;
+        public float moveSpeed = 0.1f;
+        Vector2 position = new Vector2(0f, 0f);
+        Rigidbody2D rb;
+        Transform Hub;
+        Hub root;
+        public float heightLimit;
+        LineRenderer lr;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-        Hub = GameObject.FindGameObjectWithTag("Hub").GetComponent<Transform>();
-        root = GameObject.FindGameObjectWithTag("Hub");
-    }
-    /*
-    // Update is called once per frame
-    void Update()
-    {
-        //rootPosition = root.;
-        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
-        mousePosition.x = 0;
-        if (mousePosition.y - transform.position.y < 3 && mousePosition.y - transform.position.y > -3)
+        // Start is called before the first frame update
+        void Start()
         {
-            mousePosition.y = transform.position.y;
+            rb = GetComponent<Rigidbody2D>();
+            Hub = GameObject.FindGameObjectWithTag("Hub").GetComponent<Transform>();
+            root = GameObject.FindGameObjectWithTag("Hub").GetComponent<Hub>();
         }
-        heightLimit = Hub.position.y + (Hub.localScale.y / 2) - 4;
-        position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
-    }
 
-    void FixedUpdate()
-    {
-        if (position.y < heightLimit)
+        // Update is called once per frame
+        void Update()
         {
-            rb.MovePosition(position);
+            lr = root.followMe.GetComponent<LineRenderer>();
+            if (lr != null)
+            {
+                rootPosition = lr.GetPosition(lr.positionCount - 1);
+                rootPosition.x = 0;
+                if (rootPosition.y - transform.position.y < 3 && rootPosition.y - transform.position.y > -3)
+                {
+                    rootPosition.y = transform.position.y;
+                }
+                heightLimit = Hub.position.y + (Hub.localScale.y / 2) - 4;
+                position = Vector2.Lerp(transform.position, rootPosition, moveSpeed);
+            }
         }
-    }*/
+
+        void FixedUpdate()
+        {
+            if (position.y < heightLimit)
+            {
+                rb.MovePosition(position);
+            }
+        }
+    }
 }
