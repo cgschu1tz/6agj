6b4847366178fdc1a479153583ce9df1639cd68e
cgschu1tz <cgschultz@ksu.edu>
2020-02-15 18:58:00 -0600

    How about a chili dog?

---
+++ Assets/Scripts/JustTheTip.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JustTheTip : MonoBehaviour
+{
+    void OnCollisionStay(Collision collision)
+    {
+        Debug.Log(collision.gameObject);
+    }
+}
--- Assets/Scripts/Trophic.cs
+++ Assets/Scripts/Trophic.cs
@@ -10,33 +10,62 @@ namespace Assets.Scripts
 {
     public class Trophic : MonoBehaviour
     {
-        private float _timer = 0;
-        private List<Vector3> _points;
+        private float _timer = 0f;
+        private List<Vector3> _points3D = new List<Vector3>();
+        private List<Vector2> _points2D = new List<Vector2>();
         private int _mouseBtn = 0;
+        private LineRenderer _lineRenderer;
+        private EdgeCollider2D _edgeCollider;
+
+        public float creationRate;
+        public float proportionalGrowth;
+        public float minDistanceToGrow;
 
         void Awake()
         {
-            _points.Add(new Vector3(0, 0, 0));
+            _points3D.Add(new Vector3(0, 0, 0));
+            _points2D.Add(new Vector2(0, 0));
+            _lineRenderer = GetComponent<LineRenderer>();
+            _edgeCollider = GetComponent<EdgeCollider2D>();
         }
 
         void Update()
         {
-            if (Input.GetMouseButton(0))
+            if (Input.GetMouseButton(_mouseBtn))
             {
                 _timer += Time.deltaTime;
 
-                var lr = GetComponent<LineRenderer>();
-                var pt = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                pt.z = 0;
+                if (_timer > creationRate)
+                {
+                    _timer -= creationRate;
 
-                _points.Add(pt);
-                lr.positionCount = _points.Count;
-                lr.SetPositions(_points.ToArray());
+                    var clickPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                    clickPosition.z = 0;
+
+                    var remainingDisplacement = clickPosition - _points3D[_points3D.Count - 1];
+                    if (Vector3.Magnitude(remainingDisplacement) > minDistanceToGrow)
+                    {
+                        var newPoint = _points3D[_points3D.Count - 1] + (proportionalGrowth * remainingDisplacement);
+
+                        _points3D.Add(newPoint);
+                        _points2D.Add(newPoint);
+                    }
+                }
             }
             else
             {
                 _timer = 0;
             }
+
+            _lineRenderer.positionCount = _points3D.Count;
+            _lineRenderer.SetPositions(_points3D.ToArray());
+
+            _edgeCollider.points = _points2D.ToArray();
+        }
+
+        void OnCollisionStay(Collision collision)
+        {
+            Debug.Log(collision.gameObject);
         }
     }
 }
